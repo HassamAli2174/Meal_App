7773da0c7788e527bb9f5532663d2bce06886790
HassamAli2174 <hassamarshad021@gmail.com>
2025-05-21 18:47:43 +0500

    Enhance category selection in CategoriesScreen by filtering meals based on selected category. Update navigation to pass the selected category title to MealsScreen.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,46 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/data/dummy_data.dart';
 import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/grid_category_item.dart';
+import 'package:meal_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext ctx) {
+  void _selectCategory(BuildContext ctx, Category category) {
+    final filteredMeal =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+
     Navigator.of(ctx).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'Some title', meals: []),
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredMeal),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Pick Your Category')),
-        body: GridView(
-          padding: const EdgeInsets.all(24),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
-          children: [
-            // 1st way
-            ...availableCategories.map(
-              (category) => GridCategoryItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context);
-                },
-              ),
-            ),
-            //2nd way
-            // for(final category in availableCategories )
-            // GridCategoryItem(category: category)
-          ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick Your Category')),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
         ),
+        children: [
+          // 1st way
+          ...availableCategories.map(
+            (category) => GridCategoryItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
+          ),
+          //2nd way
+          // for(final category in availableCategories )
+          // GridCategoryItem(category: category)
+        ],
       ),
     );
   }
