fe8cd0b6c18bc605d0bf852ef29a9f16925cd246
HassamAli2174 <hassamarshad021@gmail.com>
2025-05-21 15:34:28 +0500

    Initial commit; Created Models for different Categories more over have build default gridview for frontscreen display

---
+++ lib/widgets/grid_category_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GridCategoryItem extends StatelessWidget {
+  const GridCategoryItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
