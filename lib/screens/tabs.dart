67c8194570c00508624df3c5c2728b5ef1c744e9
HassamAli2174 <hassamarshad021@gmail.com>
2025-07-07 18:09:24 +0500

    Add filterMealsProvider to encapsulate meal filtering logic based on active filters. Update TabsScreen to utilize filterMealsProvider for improved state management and cleaner code structure.

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:meal_app/models/meal.dart';
 import 'package:meal_app/provider/favorites_provider.dart';
 import 'package:meal_app/provider/filters_provider.dart';
-import 'package:meal_app/provider/meals_provider.dart';
 import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/filters.dart';
 import 'package:meal_app/screens/meals.dart';
@@ -70,24 +69,8 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filterProvider);
-    final availableMeals =
-        meals.where((meal) {
-          if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-            return false;
-          }
-          if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-            return false;
-          }
-          if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-            return false;
-          }
-          if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-            return false;
-          }
-          return true;
-        }).toList();
+    final availableMeals = ref.watch(filterMealsProvider);
+
     Widget activePage = CategoriesScreen(
       // onToggleFavorite: _toggleMealFavoriteStatus,
       availableMeals: availableMeals,
